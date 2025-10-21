1d3b957acf01ed28a69e8bc24cfca0f4c40eecde
gordon023 <ngnard.123@gmail.com>
2025-10-21 22:50:54 +0800

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://your-backend-name.onrender.com/api"; // change after deploy
+const API_URL = "https://realtime-dashboard-qlut.onrender.com/api"; // change after deploy
 let currentUser = null;
 
 // --- Login/Register ---
